a34fcddf9241eaf6e8363f8917b2b956be96f9c4
Alex Micharski <amicharski@outlook.com>
2024-04-26 15:35:33 -0400

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import { useState } from 'react'
 import './App.css'
+import { Question } from "./components/Question.jsx";
+import { generatePlaceValueQuestion } from "./lib/questions/generated/index.js";
 
 function App() {
   const [score, setScore] = useState(0)
+  const [questionCount, setQuestionCount] = useState(1)
+
+  generatePlaceValueQuestion()
 
   return (
     <>
+      {questionCount < 10 ?
+          <>
 
+          </> :
+          <>
+            Final Score: {score}
+          </>}
     </>
   )
 }
